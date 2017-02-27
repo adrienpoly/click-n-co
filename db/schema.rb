de79e396870e6e0580ac5fd3aa67171649192b76
Lucie La Sagna <lucie.lasagna@essec.edu>
2017-02-27 16:58:25 +0100

    orders controller and new records for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227130743) do
+ActiveRecord::Schema.define(version: 20170227155401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ordered_products", force: :cascade do |t|
+    t.integer  "product_id"
+    t.float    "quantity"
+    t.float    "order_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["product_id"], name: "index_ordered_products_on_product_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.float    "total_price"
+    t.integer  "ordered_product_id"
+    t.datetime "pick_up_at"
+    t.integer  "user_id"
+    t.text     "instructions"
+    t.string   "status"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["ordered_product_id"], name: "index_orders_on_ordered_product_id", using: :btree
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer  "product_category_id"
+    t.string   "name"
+    t.text     "description"
+    t.string   "short_description"
+    t.float    "price"
+    t.string   "measurement_units"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["product_category_id"], name: "index_products_on_product_category_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +68,17 @@ ActiveRecord::Schema.define(version: 20170227130743) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "f_name"
+    t.string   "l_name"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "role"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "ordered_products", "products"
+  add_foreign_key "orders", "ordered_products"
+  add_foreign_key "orders", "users"
+  add_foreign_key "products", "product_categories"
 end
