3b4bde277ab3de9984b4f950e6a9ab326dbbf1fe
Adrien Poly <adrienpoly@gmail.com>
2017-02-27 18:58:42 +0100

    added geocoding to shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -6,4 +6,6 @@ class Shop < ApplicationRecord
   validates :address, presence: true
   validates :phone_number, presence: true
   validates :category, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170227155401) do
+ActiveRecord::Schema.define(version: 20170227175518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +78,8 @@ ActiveRecord::Schema.define(version: 20170227155401) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "product_id"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["product_id"], name: "index_shops_on_product_id", using: :btree
     t.index ["user_id"], name: "index_shops_on_user_id", using: :btree
   end
