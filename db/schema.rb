c95777e6769c1ed1cf9d4baf173ae3263458ac2d
Adrien Poly <adrienpoly@gmail.com>
2017-03-01 14:39:57 +0100

    added status to order as an enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301104556) do
+ActiveRecord::Schema.define(version: 20170301125100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,10 +47,10 @@ ActiveRecord::Schema.define(version: 20170301104556) do
     t.datetime "pick_up_at"
     t.integer  "user_id"
     t.text     "instructions"
-    t.string   "status"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "shop_id"
+    t.integer  "status",       default: 1, null: false
     t.index ["shop_id"], name: "index_orders_on_shop_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
