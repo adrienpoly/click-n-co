eecd83505c153366de72cb714a91a75a4ca58f24
Adrien Poly <adrienpoly@gmail.com>
2017-03-02 15:42:09 +0100

    add reduce and remove in Ajax

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,13 +15,11 @@ class OrdersController < ApplicationController
       order.save
 
       orders.each do |product_id, product|
-        ordered_product = OrderedProduct.new(order: order,
-          product_id: product_id, quantity: product['qty'],
+        ordered_product = OrderedProduct.new(
+          order: order,
+          product_id: product_id,
+          quantity: product['qty'],
           order_price: product['price'])
-        # ordered_product.order = order
-        # ordered_product.product_id = product_id
-        # ordered_product.quantity = product['qty']
-        # ordered_product.order_price = product['price']
         ordered_product.save
         sum += ordered_product.order_price * ordered_product.quantity
       end
