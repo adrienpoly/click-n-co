a3c268a3538b4f6dbca6bbfda8821d8c3a949dfa
Lucie La Sagna <lucie.lasagna@essec.edu>
2017-03-06 13:05:26 +0100

    add tabs in retailer view and some adjustments

--- app/controllers/retailer/orders_controller.rb
+++ app/controllers/retailer/orders_controller.rb
@@ -1,7 +1,11 @@
 class Retailer::OrdersController < ApplicationController
 
+  def index
+  end
+
   def show
     @order = Order.find(params[:id])
+    @shop = Shop.find(params[:shop_id])
     #@orders = @shop.orders
     #@orders = @shop.orders
   end
